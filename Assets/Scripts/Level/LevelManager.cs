cd8e54d7e5aae11f8fbd9fad6a3349d0881b8496
FrancoisDeramaux <deramaux.francois@gmail.com>
2015-11-24 11:03:35 +0100

    Level Generator

---
+++ Assets/Scripts/Level/LevelManager.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour
+{
+	void Generate(){}
+}
+
+public class LevelPattern : MonoBehaviour
+{
+	void Generate(){}
+}
+
+public class MusicManager : MonoBehaviour
+{
+	void DynamicSoundLevel(){}
+}
+
+public class BackgroundManager : MonoBehaviour
+{
+	void Generate(){}
+	void HorizontalScrolling(){}
+	void DynamicColor(){}
+}
+
+public class Tile : MonoBehaviour
+{
+}
+
+public class EdgeTileFilled : MonoBehaviour
+{
+}
+
+public class EdgeTileBaseHalf : MonoBehaviour
+{
+}
+
+public class EdgeTileTipHalf : MonoBehaviour
+{
+}
+
+public class PylonTile : MonoBehaviour
+{
+}
+
+public class ElectricPoleTile : MonoBehaviour
+{
+}
+
+public class PipeTile : MonoBehaviour
+{
+}
+
+public class BuildingTile : MonoBehaviour
+{
+}
+
+public class MovableTile : MonoBehaviour
+{
+	void Move(){}
+}
+
+public class LiftTile : MonoBehaviour
+{
+	void AutoMove(){}
+}
+
+public class PistonTile : MonoBehaviour
+{
+	void AutoMove(){}
+}
+
+public class AcidTile : MonoBehaviour
+{
+}
+
+public class PikeTile : MonoBehaviour
+{
+}
+
+public class DecorationTile : MonoBehaviour
+{
+}
+
+public class Bonus : MonoBehaviour
+{
+}
+
+public class DustBonus : MonoBehaviour
+{
+}
+
+public class GramophoneBonus : MonoBehaviour
+{
+}
+
+public class CheckPointBonus : MonoBehaviour
+{
+}
+
+public class EndBonus : MonoBehaviour
+{
+}
