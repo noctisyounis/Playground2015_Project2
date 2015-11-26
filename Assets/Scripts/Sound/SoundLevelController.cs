9eb4fe00eac28dc00743c483fef31c268efdc32c
Guillaume <guillaume.dx@gmail.com>
2015-11-26 21:50:42 +0100

    Sound Design - Update Level Balance
    Volume balance between scratches and song affected by % of items
    collected

--- Assets/Scripts/Sound/SoundLevelController.cs
+++ Assets/Scripts/Sound/SoundLevelController.cs
@@ -4,31 +4,40 @@ using System.Collections;
 public class SoundLevelController : MonoBehaviour
 {
 
-
-
+    #region Public Properties
+    public AudioSource m_AudioSource1;
+    public AudioSource m_AudioSource2;
+    public float m_TotalCount;
+    public float m_GrabbedCount;
+    #endregion
 
     void Start ()
     {
-        
-	
-	}
-	
+        m_AudioSource1.volume = 0.00f;
+        m_AudioSource2.volume = 1.00f;
 
-	void Update ()
+        m_AudioSource1.Play();
+        m_AudioSource2.Play();
+    }
+
+
+    void FixedUpdate ()
     {
-	    
+        Balance(m_TotalCount,m_GrabbedCount);
 	}
 
 
-    void OnMouseDown()
+    void Balance(float totalCount, float grabbedCount)
     {
-        
+        float BalanceRate;
+        BalanceRate = grabbedCount / totalCount;
+
+        m_AudioSource1.volume = 0.00f + BalanceRate;
+        m_AudioSource2.volume = 1.00f - BalanceRate;
     }
 
 
     #region Private properties
-    AudioClip m_SpeakerSong;
-    AudioClip m_SpeakerScratches;
     #endregion
 
 }
