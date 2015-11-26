283b9f5450f5ea5155ebc6110c74c63344e34248
Guillaume <guillaume.dx@gmail.com>
2015-11-26 16:43:47 +0100

    Sound Design - mix sound level

---
+++ Assets/Scripts/Sound/AudioClipController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioClipController : MonoBehaviour {
+
+
+	void Start () {
+        m_SpeakerSong = GetComponent<AudioSource>();
+        m_SpeakerSong.volume = 0f;
+	}
+	
+
+	void FixedUpdate () {
+
+ 
+	}
+
+
+    void OnMouseDown()
+    {
+        m_SpeakerSong.volume += 0.1f;
+    }
+
+
+    #region Private properties
+
+    AudioSource m_SpeakerSong;
+
+    #endregion
+}
---
+++ Assets/Scripts/Sound/SoundLevelController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundLevelController : MonoBehaviour
+{
+
+
+
+
+    void Start ()
+    {
+        
+	
+	}
+	
+
+	void Update ()
+    {
+	    
+	}
+
+
+    void OnMouseDown()
+    {
+        
+    }
+
+
+    #region Private properties
+    AudioClip m_SpeakerSong;
+    AudioClip m_SpeakerScratches;
+    #endregion
+
+}
