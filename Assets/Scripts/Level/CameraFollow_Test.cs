20d2a042b49e3eb1c0031592e5fc857c37914bd8
FrancoisDeramaux <deramaux.francois@gmail.com>
2015-11-27 16:43:44 +0100

    Parallax + GameManager
    Parallax - LifeManager - GramoManager - DustManager - BWEffect

---
+++ Assets/Scripts/Level/CameraFollow_Test.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow_Test : MonoBehaviour
+{
+
+	public GameObject player;
+
+	void Start () 
+	{
+		player = GameObject.FindGameObjectWithTag ("Player");	
+	}
+
+	void FixedUpdate () 
+	{
+		float posX = player.transform.position.x;
+		transform.position = new Vector3 (posX, transform.position.y, transform.position.z);
+	}
+}
